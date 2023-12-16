db061e3202c00c383f2a22b65b06e6643bfda7a1
Anth0-12 <antho62680@live.fr>
2023-12-16 01:31:28 +0100

    Simplification de la fonction

--- script.js
+++ script.js
@@ -5,25 +5,19 @@ const ChoixVoir = document.querySelector(".Choix_A_Voir");
 const ChoixVenir = document.querySelector(".Choix_A_Venir");
 const AfficheFilm = document.querySelector("#affiche");
 
-$(".film-button").on("click",function(event){
+$(".film-button").on("click", function(event){
     event.preventDefault();
     if (".film-input" !== null && ChoixVoir.checked ){
-        let nouvelleDiv = document.createElement("div");
+        let nouvelleDiv = $('<div><p class="texte" name="modifier" >' + filmInput.value + '</p>' + 
+        '<img class="image" src=' + AfficheFilm.value + '></img></div>');
         nouvelleDiv.className = "film";
-        nouvelleDiv.innerHTML =
-                "<p class='tache' name='modifier' >" +
-                filmInput.value +
-                "</p>" + "<img class='image' src=" + AfficheFilm.value + "></img>";
        $('.A_Voir').append(nouvelleDiv);
        $(".A_Voir").css("height", "100px");
        $(".film").css("height", "50px");
 } else if(".film-input" !== null && ChoixVenir.checked) {
-    let nouvelleDiv = document.createElement("div");
-            nouvelleDiv.className = "film";
-            nouvelleDiv.innerHTML =
-                    "<p class='tache' name='modifier' >" +
-                    filmInput.value +
-                    "</p>";
+    let nouvelleDiv = $('<div><p class="texte" name="modifier" >' + filmInput.value + '</p>' + 
+        '<img class="image" src=' + AfficheFilm.value + '></img></div>');
+        nouvelleDiv.className = "film";
         $('.A_Venir').append(nouvelleDiv);
         $(".A_Venir").css("height", "100px");
        $(".film").css("height", "50px");
