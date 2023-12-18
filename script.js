8650a5bb69858e3958bd6f496ddad04abaf5e906
Anth0-12 <antho62680@live.fr>
2023-12-18 23:51:57 +0100

    Ajout affichage catégories

--- script.js
+++ script.js
@@ -4,23 +4,24 @@ const A_Venir = $( '<div class="A_Venir"></div>' );
 const ChoixVoir = document.querySelector(".Choix_A_Voir");
 const ChoixVenir = document.querySelector(".Choix_A_Venir");
 const AfficheFilm = document.querySelector("#affiche");
+const listeCategories = document.querySelector("#listeCategories");
 
 $(".film-button").on("click", function(event){
     event.preventDefault();
     if (".film-input" !== null && ChoixVoir.checked ){
-        let nouvelleDiv = $('<div><p class="texte" name="modifier" >' + filmInput.value + '</p>' + 
+        let nouvelleDiv = $('<div class="ficheFilm"><p class="titreFilm" name="modifier" >' + filmInput.value + '</p>' 
+        + '<p class="categorieFilm" name="modifier" >' + listeCategories.value + '</p>' + 
         '<img class="image" src=' + AfficheFilm.value + '></img></div>');
-        nouvelleDiv.className = "film";
-       $('.A_Voir').append(nouvelleDiv);
-       $(".A_Voir").css("height", "100px");
-       $(".film").css("height", "50px");
+        $('.A_Voir').append(nouvelleDiv);
+        $(".A_Voir").css("height", "100px");
+        $(".film").css("height", "50px");
 } else if(".film-input" !== null && ChoixVenir.checked) {
-    let nouvelleDiv = $('<div><p class="texte" name="modifier" >' + filmInput.value + '</p>' + 
+    let nouvelleDiv = $('<div class="ficheFilm"><p class="titreFilm" name="modifier" >' + filmInput.value + '</p>'  
+        + '<p class="categorieFilm" name="modifier" >' + listeCategories.value + '</p>' + 
         '<img class="image" src=' + AfficheFilm.value + '></img></div>');
-        nouvelleDiv.className = "film";
         $('.A_Venir').append(nouvelleDiv);
         $(".A_Venir").css("height", "100px");
-       $(".film").css("height", "50px");
+        $(".film").css("height", "50px");
         console.log("clic")
 } else {
     console.log("erreur");
