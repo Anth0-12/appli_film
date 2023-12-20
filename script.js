99ba4461c9bd436f20a3eb7e1db95be9dd39d0fb
Anth0-12 <antho62680@live.fr>
2023-12-21 00:11:52 +0100

    Ajout d'espace pour plus de lisibilité

--- script.js
+++ script.js
@@ -10,21 +10,27 @@ let compteurDiv = 1; // Variable qui permet de compter le nombre de films
 // Sur le clic du bouton envoyer 
 $(".film-button").on("click", function(event){
     event.preventDefault(); // Empeche la page de se recharger
+
     if (".film-input" !== null && ChoixVoir.checked ){ // Vérifie que le film a bien été saisi dans la barre de recherche et que la case "A voir" est coche
         $("<div></div>", {"id": `numDiv${compteurDiv}`, // Crée une div pour chaque film avec un id différent
                           "class": "ficheFilm"}).appendTo('.A_Voir'); // Ajoute la div au conteneur
+                           
         $(`#numDiv${compteurDiv}`).html('<p class="titreFilm" name="modifier" >' + filmInput.value + '</p>' 
             + '<p class="categorieFilm" name="modifier">' + "Catégorie: " + listeCategories.value + '</p>' 
             + '<div class="boutons"><button class="modifier">' + "Modifier" + '</button>' + '<button class="supprimer">' + "Supprimer" + '</button>'
             +'<button>' + "Vu" + '</button>'); // Ajoute le contenu
+             
         compteurDiv++; // Incrémente le compteur de div
-} else if(".film-input" !== null && ChoixVenir.checked) { // On fait pareil mais pour la case "A venir"
+}
+else if(".film-input" !== null && ChoixVenir.checked) { // On fait pareil mais pour la case "A venir"
         $("<div></div>", {"id": `numDiv${compteurDiv}`, 
                           "class": "ficheFilm"}).appendTo('.A_Venir');
+
         $(`#numDiv${compteurDiv}`).html('<p class="titreFilm" name="modifier" >' + filmInput.value + '</p>' 
             + '<p class="categorieFilm" name="modifier">' + "Catégorie: " + listeCategories.value + '</p>' 
             + '<div class="boutons"><button>' + "Modifier" + '</button>' + '<button class="supprimer">' + "Supprimer" + '</button>'
             +'<button>' + "Vu" + '</button>');
+            
         compteurDiv++;
 } else {
     console.log("erreur");
